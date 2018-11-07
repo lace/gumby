ceaf45de623012c91519e5700d3941035bc8298b
Paul Melnikow <email@paulmelnikow.com>
2018-11-07 08:09:38 -0500

    Set up CircleCI

--- dev.py
+++ dev.py
@@ -1,7 +1,25 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
+import os
 import click
 from executor import execute
+
+
+def nose_cmd():
+    # More reliably locate this command when more than one Python are
+    # installed.
+    try:
+        execute("nose2-2.7 --help", capture=True)
+        return "nose2-2.7"
+    except:
+        return "nose2"
+
+
+def python_source_files():
+    import glob
+
+    return glob.glob("*.py") + ["gumby/"]
+
 
 @click.group()
 def cli():
@@ -9,11 +27,27 @@ def cli():
 
 @cli.command()
 def init():
-    execute('pip install -r requirements_dev.txt')
+    execute("pip install --upgrade -r requirements_dev.txt")
 
 @cli.command()
 def test():
-    execute('nose2')
+    execute(nose_cmd())
+
+
+@cli.command()
+def lint():
+    execute("pyflakes", *python_source_files())
+
+
+@cli.command()
+def black():
+    execute("black", *python_source_files())
+
+
+@cli.command()
+def black_check():
+    execute("black", "--check", *python_source_files())
+
 
 @cli.command()
 def upload():
@@ -22,4 +56,5 @@ def upload():
     execute('twine upload dist/*')
 
 if __name__ == '__main__':
+    os.chdir(os.path.abspath(os.path.dirname(__file__)))
     cli()
