44ee1c4c319b6621dedcb0835906e0b5a0ffd209
Paul Melnikow <email@paulmelnikow.com>
2018-11-07 11:12:05 -0500

    Working

--- gumby/core.py
+++ gumby/core.py
@@ -1,6 +1,8 @@
-def stretch_segment_along_y(mesh, in_range, delta):
-    import numpy as np
+from __future__ import print_function
+import vx
+import numpy as np
 
+def stretch_segment_along_y(mesh, in_range, delta):
     height = mesh.v[:, 1]
     minval, maxval = in_range
 
@@ -26,8 +28,6 @@ def stretch_segments_along_y(mesh, landmarks, segments):
     return mesh
 
 def principal_components(coords):
-    import numpy as np
-
     mean = np.mean(coords, axis=0)
     _, _, result = np.linalg.svd(coords - mean)
     return result
@@ -36,13 +36,9 @@ def major_axis(coords):
     return principal_components(coords)[0]
 
 def project_to_line(points, point_on_line, direction):
-    import vx
-
     return point_on_line + vx.proj(points - point_on_line, direction)
 
 def scalar_project_to_line(points, point_on_line, direction):
-    import vx
-
     return vx.sproj(points - point_on_line, direction)
 
 def tug(mesh, groups, axis, reference_point, travel, tug_range, easing, anchor_point):
@@ -69,45 +65,68 @@ def tug(mesh, groups, axis, reference_point, travel, tug_range, easing, anchor_p
         easing_attack = easing_decay = easing
 
     def tweened_sproj(s):
-        if s < sproj_reference_point:
+        if s < sproj_start:
+            return 0.0
+        elif s < sproj_reference_point:
             t = (s - sproj_start) / (sproj_reference_point - sproj_start)
             return easing_attack(t)
-        elif s > sproj_reference_point:
-            t = (s - sproj_reference_point) / (sproj_end - sproj_reference_point)
+        elif s == sproj_reference_point:
+            return 1.0
+        elif s > sproj_reference_point and s <= sproj_end:
+            t = (s - sproj_end) / (sproj_reference_point - sproj_end)
             return easing_decay(t)
         else:
-            return 1.0
+            return 0.0
+
+    def dbg(s):
+        print("tweened_sproj({}): {}".format(s, tweened_sproj(s)))
+
+    for i in range(100):
+        dbg(-i/100.)
+
+
+    # dbg(sproj_start)
+    # dbg(0.5*(sproj_reference_point + sproj_start))
+    # dbg(sproj_reference_point)
+    # dbg(0.5*(sproj_end + sproj_reference_point))
+    # dbg(sproj_end)
+
+    # import sys
+    # sys.exit(1)
+
+    # import pdb
+    # pdb.set_trace()
 
     sproj_points = scalar_project_to_line(mesh.v, centroid, axis)
-    travel = np.array([tweened_sproj(s) for s in sproj_points])
+    travel_for_each_point = travel * np.array([tweened_sproj(s) for s in sproj_points])
 
     proj_points = project_to_line(mesh.v, centroid, axis)
-    from_axis_to_points = mesh.v - centroid - proj_points
+    from_axis_to_points = mesh.v - proj_points
 
-    mesh.v = mesh.v + travel * from_axis_to_points
+    mesh.v = mesh.v + travel_for_each_point.reshape(-1, 1) * from_axis_to_points
 
 def main():
     """
     python -m gumby.core
     """
-    from lace.mesh import Mesh
     import pytweening
+    from lace.mesh import Mesh
     from .path import relative_to_project
 
-    original_mesh = Mesh(filename=relative_to_project("glass-bottle/glass_bottle.obj"))
+    original_mesh = Mesh(filename=relative_to_project("examples/glass-bottle/glass_bottle.obj"))
     # Fix crash in write_obj.
+    del original_mesh.vn
     del original_mesh.segm
     mesh = original_mesh.copy()
 
-    tug(
-        mesh=mesh,
+    tug(mesh=mesh,
         groups=None,
         axis=vx.basis.y,
         reference_point=np.array([0.0, 0.375, 0.0]),
-        travel=0.1,
-        tug_range=np.array([[0.0, 0.10, 0.0], [0.0, 0.65, 0.0]],
+        travel=0.25,
+        tug_range=np.array([[0.0, 0.10, 0.0], [0.0, 0.65, 0.0]]),
         easing=pytweening.easeInOutSine,
-        anchor_points=None)
+        anchor_point=None)
 
 
     mesh.show()
